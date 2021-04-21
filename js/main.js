27651e224d5128476f655dba5e02c87648cb6b3b
Jandy Kiger <76858411+jandyrae@users.noreply.github.com>
2021-04-21 15:56:05 -0700

    finished the story editor linked to index

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
 
-const links = [{
-    label: "Week1 notes",
-    url: "week1/index.html"
-}];
+// const links = [{
+//     label: "Week1 notes",
+//     url: "week1/index.html"
+// }];
 
-function makeList(){
+// function makeList(){
 
-var listContainer = document.createElement('div');
-var listElement = document.createElement('ol');
-var numListItems = links.length;
-var listItem;
-var i;
+// var listContainer = document.createElement('div');
+// var listElement = document.createElement('ol');
+// var numListItems = links.length;
+// var listItem;
+// var i;
 
-document.getElementsByTagName('body')[0].appendChild(listContainer);
-listContainer.appendChild(listElement);
+// document.getElementsByTagName('body')[0].appendChild(listContainer);
+// listContainer.appendChild(listElement);
 
-for (i = 0; i < numListItems; i++) {
-//create line items, populate them, and add list item to list element
-    listItem = document.createElement('li');
-    listItem.innerHTML = links[i];
-    listElement.appendChild(listItem);
+// for (i = 0; i < numListItems; i++) {
+// //create line items, populate them, and add list item to list element
+//     listItem = document.createElement('li');
+//     listItem.innerHTML = links[i];
+//     listElement.appendChild(listItem);
     
-}
-}
+// }
+// }
