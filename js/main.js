e01f0ab216b106fe58a2cfa868a2e91abfa4ca51
Jandy Kiger <76858411+jandyrae@users.noreply.github.com>
2021-04-23 16:44:52 -0700

    removed / to attach the image

--- js/main.js
+++ js/main.js
@@ -23,3 +23,11 @@
     
 // }
 // }
+
+
+var li = document.createElement('li'),
+        anchor = document.createElement('a');
+        anchor.href = a.url;
+        anchor.text = a.name;
+        li.appendChild(anchor);
+        ul.appendChild(li);
