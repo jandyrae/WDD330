2c46d5663bf770aaf7277d71dacaef16356a57f5
Jandy Kiger <76858411+jandyrae@users.noreply.github.com>
2021-05-10 19:00:57 -0700

    started team activity ticTacToe

---
+++ week4/ticTacToe.js
@@ -0,0 +1,21 @@
+// set variables for the elements used in the game
+const chooseTL = document.getElementById('topLeft');
+const chooseTM = document.getElementById('topMid');
+const chooseTR = document.getElementById('topRight');
+const chooseML = document.getElementById('midLeft');
+const chooseMM = document.getElementById('midMid');
+const chooseMR = document.getElementById('midRight');
+const chooseBL = document.getElementById('bottomLeft');
+const chooseBM = document.getElementById('bottomMid');
+const chooseBR = document.getElementById('bottomRight');
+
+
+function playGame(){
+    // if clicked enter X or O
+}
+
+chooseBR.addEventListener('click',() => chooseBR.style.visibility = "visible");
+
+function reset(){
+    // clear the changes from gameplay
+}
