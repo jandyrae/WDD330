25b38f52bb72c006540039c8c1f2c6b1358d0635
Jandy Kiger <76858411+jandyrae@users.noreply.github.com>
2021-05-12 20:08:22 -0700

    team activity upload (tictactoe)

--- week4/ticTacToe.js
+++ week4/ticTacToe.js
@@ -1,21 +1,41 @@
-// set variables for the elements used in the game
-const chooseTL = document.getElementById('topLeft');
-const chooseTM = document.getElementById('topMid');
-const chooseTR = document.getElementById('topRight');
-const chooseML = document.getElementById('midLeft');
-const chooseMM = document.getElementById('midMid');
-const chooseMR = document.getElementById('midRight');
-const chooseBL = document.getElementById('bottomLeft');
-const chooseBM = document.getElementById('bottomMid');
-const chooseBR = document.getElementById('bottomRight');
+const chooseTL = document.getElementById("topLeft");
+const chooseTM = document.getElementById("topMid");
+const chooseTR = document.getElementById("topRight");
+const chooseML = document.getElementById("midLeft");
+const chooseMM = document.getElementById("midMid");
+const chooseMR = document.getElementById("midRight");
+const chooseBL = document.getElementById("bottomLeft");
+const chooseBM = document.getElementById("bottomMid");
+const chooseBR = document.getElementById("bottomRight");
+
+const board = document.getElementById("board");
+
+let count = 0;
+
+function playGame() {
+  // if clicked enter X or O
+}
+chooseTR.addEventListener("click", play);
+chooseTR.addEventListener("click", play);
+chooseTR.addEventListener("click", play);
+chooseML.addEventListener("click", play);
+chooseMM.addEventListener("click", play);
+chooseMR.addEventListener("click", play);
+chooseBL.addEventListener("click", play);
+chooseBM.addEventListener("click", play);
+chooseBR.addEventListener("click", play);
 
 
-function playGame(){
-    // if clicked enter X or O
+function play() {
+  if (count % 1 == 0) {
+    let X = document.createTextNode("X");
+    chooseBR.appendChild(X);
+  } else {
+    let O = document.createTextNode("O");
+    chooseBR.appendChild(O);
+  }
 }
 
-chooseBR.addEventListener('click',() => chooseBR.style.visibility = "visible");
-
-function reset(){
-    // clear the changes from gameplay
+function reset() {
+  // clear the changes from gameplay
 }
