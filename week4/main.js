fcd6e9211d9e4e663a475f646168449a422c027e
Jandy Kiger <76858411+jandyrae@users.noreply.github.com>
2021-05-18 13:16:32 -0700

    foundational code for week 5

--- week4/main.js
+++ week4/main.js
@@ -5,21 +5,21 @@ fetch(url)
   .then(quiz => {
     view.start.addEventListener('click', () => game.start(quiz.questions), false);
     view.response.addEventListener('click', (event) => game.check(event), false);
-});
+  });
 
 // Utility functions
-function random(a,b=1) {
-// if only 1 argument is provided, we need to swap the values of a and b
+function random(a, b = 1) {
+  // if only 1 argument is provided, we need to swap the values of a and b
   if (b === 1) {
-	  [a,b] = [b,a];
+    [a, b] = [b, a];
   }
-  return Math.floor((b-a+1) * Math.random()) + a;
+  return Math.floor((b - a + 1) * Math.random()) + a;
 }
 
 function shuffle(array) {
   for (let i = array.length; i; i--) {
-      let j = random(i)-1;
-      [array[i - 1], array[j]] = [array[j], array[i - 1]];
+    let j = random(i) - 1;
+    [array[i - 1], array[j]] = [array[j], array[i - 1]];
   }
 }
 
@@ -33,97 +33,100 @@ const view = {
   response: document.querySelector('#response'),
   timer: document.querySelector('#timer strong'),
   hiScore: document.querySelector('#hiScore strong'),
-  render(target,content,attributes) {
-      for(const key in attributes) {
-        target.setAttribute(key, attributes[key]);
-      }
-      target.innerHTML = content;
+  render(target, content, attributes) {
+    for (const key in attributes) {
+      target.setAttribute(key, attributes[key]);
+    }
+    target.innerHTML = content;
   },
-  show(element){
+  show(element) {
     element.style.display = 'block';
   },
-  hide(element){
+  hide(element) {
     element.style.display = 'none';
   },
-  setup(){
+  setup() {
     this.show(this.question);
     this.show(this.response);
     this.show(this.result);
     this.hide(this.start);
-    this.render(this.score,game.score);
-    this.render(this.result,'');
-    this.render(this.info,'');
+    this.render(this.score, game.score);
+    this.render(this.result, '');
+    this.render(this.info, '');
     this.render(this.hiScore, game.hiScore());
   },
-  teardown(){
+  teardown() {
     this.hide(this.question);
     this.hide(this.response);
     this.show(this.start);
     this.render(this.hiScore, game.hiScore());
   },
-  buttons(array){
+  buttons(array) {
     return array.map(value => `<button>${value}</button>`).join('');
   }
 };
 
 const game = {
-  start(quiz){
+  start(quiz) {
     console.log('start() invoked');
     this.score = 0;
     this.questions = [...quiz];
     view.setup();
     this.secondsRemaining = 20;
-    this.timer = setInterval( this.countdown , 1000 );
+    this.timer = setInterval(this.countdown, 1000);
     this.ask();
   },
   countdown() {
     game.secondsRemaining--;
-    view.render(view.timer,game.secondsRemaining);
-      if(game.secondsRemaining <= 0) {
-        game.gameOver();
-      }
+    view.render(view.timer, game.secondsRemaining);
+    if (game.secondsRemaining <= 0) {
+      game.gameOver();
+    }
   },
-  ask(name){
+  ask(name) {
     console.log('ask() invoked');
-    if(this.questions.length > 2) {
+    if (this.questions.length > 2) {
       shuffle(this.questions);
       this.question = this.questions.pop();
       const options = [this.questions[0].realName, this.questions[1].realName, this.question.realName];
       shuffle(options);
       const question = `What is ${this.question.name}'s real name?`;
-      view.render(view.question,question);
-      view.render(view.response,view.buttons(options));
-    }
-    else {
+      view.render(view.question, question);
+      view.render(view.response, view.buttons(options));
+    } else {
       this.gameOver();
     }
   },
-  check(event){
+  check(event) {
     console.log('check(event) invoked');
     const response = event.target.textContent;
     const answer = this.question.realName;
-    if(response === answer){
+    if (response === answer) {
       console.log('correct');
-      view.render(view.result,'Correct!',{'class':'correct'});
+      view.render(view.result, 'Correct!', {
+        'class': 'correct'
+      });
       this.score++;
-      view.render(view.score,this.score);
+      view.render(view.score, this.score);
     } else {
       console.log('wrong');
-      view.render(view.result,`Wrong! The correct answer was ${answer}`,{'class':'wrong'});
+      view.render(view.result, `Wrong! The correct answer was ${answer}`, {
+        'class': 'wrong'
+      });
     }
     this.ask();
   },
-  gameOver(){
+  gameOver() {
     console.log('gameOver() invoked');
-    view.render(view.info,`Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
+    view.render(view.info, `Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
     view.teardown();
     clearInterval(this.timer);
   },
-  hiScore(){
+  hiScore() {
     const hi = localStorage.getItem('highScore') || 0;
-    if(this.score > hi || hi === 0) {
-      localStorage.setItem('highScore',this.score);
-      view.render(view.info,'** NEW HIGH SCORE! **');
+    if (this.score > hi || hi === 0) {
+      localStorage.setItem('highScore', this.score);
+      view.render(view.info, '** NEW HIGH SCORE! **');
     }
     return localStorage.getItem('highScore');
   }
--- week4/week4.js
+++ week4/week4.js
@@ -166,3 +166,39 @@ function mean(array) {
 // }
 // then this could be imported with this code
 // import  { mean, variance } from './stats.js';
+
+// from chapter 15 array for todos
+const tasks = [
+    { name: 'Get Milk' },
+    { name: 'Go for a run' },
+    { name: 'Finish writing last chapter' },
+    { name: 'Phone bank' },
+    { name: 'Email Craig' }
+    ]
+// using template languages 
+{/* <ul>
+{{#tasks}}
+<li>{{name}}</li>
+{{/task}}
+</ul> */}
+//  and this 
+{/* <ul>
+    <% tasks.forEach(function(task) { %>
+    <li><%= task.name %></li>
+    <% }); %>
+    </ul>
+<% } %>> */}
+// would yeild this
+{/* <ul>
+    <li>Get Milk</li>
+    <li>Go for a run</li>
+    <li>Finish writing last chapter</li>
+    <li>Phone bank</li>
+    <li>Email Craig</li>
+</ul> */}
+// using these language templates
+// Handlebars
+// Pug
+// EJS
+// Mustache
+// Nunjucks
