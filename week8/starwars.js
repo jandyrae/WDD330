7c8a073a6a695a50a0803a3ac522d9015a93ff17
Jandy Kiger <76858411+jandyrae@users.noreply.github.com>
2021-06-09 13:58:35 -0700

    team assignment

--- week8/starwars.js
+++ week8/starwars.js
@@ -3,7 +3,7 @@ const apiPlanets = document.getElementById('planets');
 const apiPeople = document.getElementById('people');
 const apiFilms = document.getElementById('films');
 const outputDiv = document.getElementById('output');
-
+const apiShips = document.getElementById('ships');
 
 
 // the urls for each 
@@ -11,12 +11,11 @@ const outputDiv = document.getElementById('output');
 // "people": "https://swapi.dev/api/people/",
 // "planets": "https://swapi.dev/api/planets/",
 // "species": "https://swapi.dev/api/species/",
-// "starships": "https://swapi.dev/api/starships/",
+const shipAPI = "https://swapi.dev/api/starships/";
 // "vehicles": "https://swapi.dev/api/vehicles/"
 const planetAPI = 'https://swapi.dev/api/planets/5/';
 const filmsAPI = 'https://swapi.dev/api/films/';
-const peopleAPI = 'https://swapi.dev/api/people/1/';
-
+const peopleAPI = 'https://swapi.dev/api/people/';
 
 
 
@@ -46,7 +45,22 @@ apiPeople.addEventListener('click', () => {
     throw Error(response.statusText);
     })
     .then( response => response.json() )
-    .then( data => outputDiv.innerText = 'Person\'s Name: ' + data.name + ' Hair color: ' + data.hair_color + ' Eye Color: ' +  data.eye_color + ' Gender:  ' + data.gender )
+    .then (data => outputDiv.innerHTML = document.querySelector('a').setAttribute('href', data.next )) // shows the next page 
+    // .then( data => outputDiv.innerText = 'Person\'s Name: ' + data.name + ' Hair color: ' + data.hair_color + ' Eye Color: ' +  data.eye_color + ' Gender:  ' + data.gender )
     .catch( error => console.log('There was an error:', error))
 },false);
 
+apiShips.addEventListener('click', () => {
+    fetch(shipAPI)
+    .then( response => {
+        outputDiv.innerHTML = 'Waiting for response...';
+    if(response.ok) {
+        return response;
+    }
+    throw Error(response.statusText);
+    })
+    .then( response => response.json() )
+    .then (data => outputDiv.innerHTML = 'Ships Information here ' + data.results.name)
+    // .then( data => outputDiv.innerText = 'Person\'s Name: ' + data.name + ' Hair color: ' + data.hair_color + ' Eye Color: ' +  data.eye_color + ' Gender:  ' + data.gender )
+    .catch( error => console.log('There was an error:', error))
+},false);
