424f3b22aa5938b4882cbcbdfbdc45f928a5dfbc
Jandy Kiger <76858411+jandyrae@users.noreply.github.com>
2021-07-20 18:25:44 -0700

    working on next capabilities

--- week8/starwars.js
+++ week8/starwars.js
@@ -93,6 +93,7 @@ apiShips.addEventListener('click', () => {
     .then( response => response.json() )
     .then (data => {
         console.log(data);
+        document.querySelector('button').setAttribute('href', data.next );
         for(let i = 0; i < 10; i++) {
         outputDiv.innerHTML += 
         `<h1>Ship Name: ${data.results[i].name}</h1>
